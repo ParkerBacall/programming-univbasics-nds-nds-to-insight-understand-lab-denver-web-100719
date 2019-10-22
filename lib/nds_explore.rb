7c610d51a7fa42961a20187889d263002024ce50
Parker Bacall <pbacall2@gmail.com>
2019-10-22 04:39:07 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,15 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
-  nil
+  pp nds
 end
 
 def print_first_directors_movie_titles
+  p directors_database[0][:movies][0][:title]
+  p directors_database[0][:movies][1][:title]
+  p directors_database[0][:movies][2][:title]
+  p directors_database[0][:movies][3][:title]
+  p directors_database[0][:movies][4][:title]
+  p directors_database[0][:movies][5][:title]
+
 end
