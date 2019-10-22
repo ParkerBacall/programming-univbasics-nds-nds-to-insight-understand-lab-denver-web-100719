ccce6d02aa1328d1e445981972cc54d0d061d02e
Parker Bacall <pbacall2@gmail.com>
2019-10-22 04:47:30 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,7 +8,7 @@ end
 
 def print_first_directors_movie_titles
   puts directors_database[0][:movies][0][:title]
-  puts directors_database[0][:movies][0][:title]
+  puts directors_database[0][:movies][1][:title]
 
 
 end
